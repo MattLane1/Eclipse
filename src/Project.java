efa4b375a364fb347909662f9444cb7cf8794708
Matthew Lane <malane@rogers.com>
2015-01-22 15:48:00 -0500

    Added Vehicle Class

--- src/Project.java
+++ src/Project.java
@@ -1,13 +1,12 @@
 /**
- * 
- */
-
-/**
  * @author Matt
  * @version 0.1 - Created basic project
  * Program Description Demo files for Comp 1011 lession 3
+ */
+
+public class Project {
+/**
+ * Main Method for Project
  * 
  */
-public class Project {
-
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author Matt
+ *
+ */
+public class Vehicle {
+
+}
